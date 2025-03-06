6efd44acbc02efc2959d4d61168811e77857ca7d
aperegarc <adrigarciaprz@gmail.com>
2025-03-06 13:41:02 +0100

    Login

--- springsecurity/src/main/java/com/aperegarc/springsecurity/controller/DemoController.java
+++ springsecurity/src/main/java/com/aperegarc/springsecurity/controller/DemoController.java
@@ -1,6 +1,9 @@
 
 package com.aperegarc.springsecurity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class DemoController {
 
